eac357e2a41efebf1ff71dbefb910491dd9b404d
Bahtiar <zamkorong12@gmail.com>
2019-11-23 15:18:49 +0700

    add new menu in nav

--- application/views/tutor/nav_list.php
+++ application/views/tutor/nav_list.php
@@ -35,6 +35,24 @@
                                 </li>
                             </ul>
                         </li>
+                        <li class="nav-main-item">
+                            <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                                <i class="nav-main-link-icon si si-energy"></i>
+                                <span class="nav-main-link-name">Kelas</span>
+                            </a>
+                            <ul class="nav-main-submenu">
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link" href="<?=base_url('kelas/');?>">
+                                        <span class="nav-main-link-name">Daftar Kelas</span>
+                                    </a>
+                                </li>
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link" href="<?=base_url('kelas/tambah');?>">
+                                        <span class="nav-main-link-name">Tambah Kelas</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
                 <!-- END Side Navigation -->
