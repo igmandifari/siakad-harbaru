1160ab25f447ec40eb340e10a9191e135a338bd8
Bahtiar <zamkorong12@gmail.com>
2019-11-23 11:09:21 +0700

    modified at change

--- application/controllers/Tutor.php
+++ application/controllers/Tutor.php
@@ -1,29 +1,41 @@
 <?php
-class Tutor extends CI_Controller {
+class Tutor extends CI_Controller 
+{
+
     public function __construct()
     {
         parent::__construct();
         $this->load->model("Tutor_model");
+        $this->load->library('upload');
     }
-    public function index(){
+
+    public $error ="";
+    public $status ="";
+
+    public function index()
+    {
         $data["tutors"] = $this->Tutor_model->getAll();
         $data["title"] = "Data Tutor";
         $data["actor"] = "Tutor";
-         //$this->load->view('tutor/coba');
-        $this->load->view('tutor/list',$data);
-        
+    
+        $this->load->view('tutor/list',$data);    
     }
-    public function hapus($id=null){
-        if (!isset($id)) redirect('siswa');
-        
-        if ($this->Tutor_model->delete($id)) {
+
+    public function hapus($id=null)
+    {
+        if (!isset($id)){
+            redirect('siswa');
+        }
+        else{
+            $this->Tutor_model->delete($id);
             redirect('tutor');
         }
     }
+
     public function ubah($id=null)
     {
         $tutor= $this->Tutor_model;
-//        if(!isset($id)) redirect('siswa');
+        
         if(isset($_POST["submit"])){
             $tutor->perbarui();
             redirect('tutor');
@@ -31,23 +43,29 @@ class Tutor extends CI_Controller {
         else{
             $data["tutor"] = $tutor->getById($id);
             $data["title"] = "Ubah Data";
-            $data["actor"] = "Tutor";
+            $data["case"] = "Tutor";
             $this->load->view("tutor/ubah",$data);
         }
     }
+
     public function tambah(){
         $tutor = $this->Tutor_model;
         //$validasi = $this->form_validation;
         //$validasi = set_rules('field_name', 'Field Label', 'tutor_nip|NIP|required');
         //$submit = $this->input->post("submit");
+        // $config['upload_path']      = './gambar/';
+        // $config['allowed_types']    = 'jpg|png|jpeg';
+        // $config['max_size']         = '200';
+        //$upload_img = $this->upload($config)->do_upload('tutor_foto');
         if(isset($_POST["submit"])) {
-            
+            //$error = $this->upload->display_errors();
+            //$status = $this->upload->data();
             $tutor->simpan();
             redirect('tutor');
             //$this->session->set_flashdata('success', 'Berhasil disimpan');
         }
         $data["title"] = "Tambah Data";
-        $data["actor"] = "Tutor";
+        $data["case"] = "Tutor";
 
         $this->load->view('tutor/tambah',$data);
     }
