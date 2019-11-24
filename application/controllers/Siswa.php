03559053862e1cb3ba0aa3487b7092486cd93bad
Igman Difari <54891762+igmandifari@users.noreply.github.com>
2019-11-24 07:59:22 +0700

    required valdidation

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -5,137 +5,68 @@ Class Siswa extends CI_Controller {
     function __construct() {
         parent::__construct();
         //chekAksesModule();
-        $this->load->library('Ssp');
+        $this->load->library('form_validation');
         $this->load->model('Siswa_model');
+        $this->load->library('upload');
     }
 
     
-    
-    function data() {
-   
-        // nama tabel
-        $table = 'siswa';
-        // nama PK
-        $primaryKey = 'siswa_id';
-        // list field
-        $columns = array(
-            array('db' => 'siswa_id', 'dt' => 'siswa_id'),
-            array('db' => 'siswa_nis', 'dt' => 'siswa_nis'),
-            array('db' => 'siswa_nisn', 'dt' => 'siswa_nisn'),
-            array('db' => 'siswa_nama', 'dt' => 'nama'),
-            array('db' => 'siswa_tanggal_lahir', 'dt' => 'siswa_tanggal_lahir'),
-            array(
-                'db' => 'siswa_id',
-                'dt' => 'aksi',
-                'formatter' => function( $d) {
-                    return "<a href='edit.php?id=$d'>EDIT</a>";
-                    return anchor('siswa/edit/'.$d,'<i class="fa fa-edit"></i>','class="btn btn-xs btn-teal tooltips" data-placement="top" data-original-title="Edit"').' 
-                        '.anchor('siswa/delete/'.$d,'<i class="fa fa-trash"></i>','class="btn btn-xs btn-danger tooltips" data-placement="top" data-original-title="Delete"');
-                }
-            )
-        );
+    function index() { 
+        $data["title"] = "Data Siswa";
+        $data["actor"] = "Siswa";
+        $data["siswas"] = $this->Siswa_model->getAll();
 
-        $sql_details = array(
-            'user' => $this->db->username,
-            'pass' => $this->db->password,
-            'db' => $this->db->database,
-            'host' => $this->db->hostname
-        );
+        $this->load->view('siswa/list',$data);
+    }
 
-        echo json_encode(
-                SSP::simple($_GET, $sql_details, $table, $primaryKey, $columns)
-        );
+    public function hapus($id=null)
+    {
+        if (!isset($id)){
+            redirect('siswa');
+        }
+        else{
+            $this->Siswa_model->delete($id);
+            redirect('siswa');
+        }
+    }
+
+    public function ubah($id=null)
+    {
+        $siswa= $this->Siswa_model;
+        
+        if(isset($_POST["submit"])){
+            $siswa->perbarui();
+            redirect('siswa');
+        }
+        else{
+            $data["siswa"] = $siswa->getById($id);
+            $data["title"] = "Ubah Data";
+            $data["case"] = "siswa";
+            $this->load->view("siswa/ubah",$data);
+        }
+    }
+    public function tambah(){
+        $siswa = $this->Siswa_model;
+        
+        $data["title"] = "Tambah Data";
+        $data["actor"] = "Siswa";
+
+        $this->form_validation->set_rules('siswa_nis','NIS','required|numeric');
+        $this->form_validation->set_rules('siswa_nisn','NISN','required|numeric');
+        $this->form_validation->set_rules('siswa_nama','NAMA','required');
+        $this->form_validation->set_rules('siswa_jenis_kelamin','Jenis Kelamin','required');
+        $this->form_validation->set_rules('siswa_agama','Agama','required');
+        if ($this->form_validation->run() == FALSE){
+            $this->load->view('siswa/tambah',$data);
+        } else{
+
+            if(isset($_POST["submit"])) {
+                $siswa->simpan();
+                $this->session->set_flashdata('flash', 'Ditambahkan');
+            }
+            redirect('siswa');
+        }
     }
 
 
-    function index() {
-        $this->load->model('Siswa_model');
-        $data["siswa"] = $this->Siswa_model->getAll();
-        $this->template->load('template', 'siswa/list',$data);
-    }
-
-//     function add() {
-//         if (isset($_POST['submit'])) {
-//             $uploadFoto = $this->upload_foto_siswa();
-//             $this->Model_siswa->save($uploadFoto);
-//             redirect('siswa');
-//         } else {
-//             $this->template->load('template', 'siswa/add');
-//         }
-//     }
-    
-//     function edit(){
-//         if(isset($_POST['submit'])){
-//             $uploadFoto = $this->upload_foto_siswa();
-//             $this->Model_siswa->update($uploadFoto);
-//             redirect('siswa');
-//         }else{
-//             $nim           = $this->uri->segment(3);
-//             $data['siswa'] = $this->db->get_where('tbl_siswa',array('nim'=>$nim))->row_array();
-//             $this->template->load('template', 'siswa/edit',$data);
-//         }
-//     }
-    
-//     function delete(){
-//         $nim = $this->uri->segment(3);
-//         if(!empty($nim)){
-//             // proses delete data
-//             $this->db->where('nim',$nim);
-//             $this->db->delete('tbl_siswa');
-//         }
-//         redirect('siswa');
-//     }
-    
-//     function upload_foto_siswa(){
-//         $config['upload_path']          = './uploads/';
-//         $config['allowed_types']        = 'jpg|png';
-//         $config['max_size']             = 1024; // imb
-//         $this->load->library('upload', $config);
-//             // proses upload
-//         $this->upload->do_upload('userfile');
-//         $upload = $this->upload->data();
-//         return $upload['file_name'];
-//     }
-    
-    
-//     function siswa_aktif(){
-//         $this->template->load('template', 'siswa/siswa_aktif');
-//     }
-    
-//     function load_data_siswa_by_rombel(){
-//         $rombel = $_GET['rombel'];
-        
-//         echo "<table class='table table-bordered'>
-//             <tr><th width='90'>NIM</th><th>NAMA</th></tr>";
-//         $this->db->where('id_rombel',$rombel);
-//         $siswa = $this->db->get('tbl_siswa');
-//         foreach ($siswa->result() as $row){
-//             echo "<tr><td>$row->nim</td><td>$row->nama</td></tr>";
-//         }
-//         echo"</table>";
-//     }
-    
-//     function data_by_rombel_excel(){
-//         $this->load->library('CPHP_excel');
-//         $objPHPExcel = new PHPExcel();
-//         $objPHPExcel->getActiveSheet()->setCellValue('A1', 'NIM');
-//         $objPHPExcel->getActiveSheet()->setCellValue('B1', 'SISWA');
-        
-//         $rombel = $_POST['rombel'];
-//         $this->db->where('id_rombel',$rombel);
-//         $siswa = $this->db->get('tbl_siswa');
-//         $no=2;
-//         foreach ($siswa->result() as $row){
-//             $objPHPExcel->getActiveSheet()->setCellValue('A'.$no, $row->nim);
-//             $objPHPExcel->getActiveSheet()->setCellValue('B'.$no, $row->nama);
-//             $no++;
-//         }
-        
-//         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007'); 
-//         $objWriter->save("data-siswa.xlsx");
-//         $this->load->helper('download');
-//         force_download('data-siswa.xlsx', NULL);
-//     }
-
-// 
 }
