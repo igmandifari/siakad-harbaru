e3b8d64deb7e0017a9559bd6b73285bc9aded056
Bahtiar <zamkorong12@gmail.com>
2019-11-26 18:48:47 +0700

    innerjoin on object jadwal and add session

--- application/models/Jadwal_model.php
+++ application/models/Jadwal_model.php
@@ -3,6 +3,9 @@
 class Jadwal_model extends CI_Model{
 
     private $_table = "jadwal";
+    private $_tutor = "tutor";
+    private $_kelas = "kelas";
+    private $_matpel = "matpel";
 
     public function simpan()
     {
@@ -30,13 +33,22 @@ class Jadwal_model extends CI_Model{
             'jadwal_jam_berakhir'   => $this->input->post("jadwal_jam_berakhir")
 
         );
-        $this->db->update('jadwal_id',$this->input->post("id"));
+        $this->db->where('jadwal_id',$this->input->post("id"));
         return $this->db->update($this->_table, $data);
     }
     
     public function getAll()
     {
-        return $this->db->get($this->_table)->result();
+//         $this->db->select('*');
+//         $this->db->from($this->_table);
+//         $this->db->join($this->_kelas,'kelas.kelas_id=jadwal.kelas_id');
+//         $this->db->join($this->tutor,'tutor.tutor_id=jadwal.tutor_id');
+//         $this->db->join($this->_matpel,'matpel.matpel_id=jadwal.matpel_id');
+        
+//         $query = $this->db->get();
+//  return $query->result();
+            $query = $this->db->query("SELECT * FROM jadwal INNER JOIN kelas ON kelas.kelas_id=jadwal.kelas_id INNER JOIN tutor ON tutor.tutor_id=jadwal.tutor_id INNER JOIN matpel ON matpel.matpel_id=jadwal.matpel_id");
+            return $query->result();
     }
 
     public function getById($id)
