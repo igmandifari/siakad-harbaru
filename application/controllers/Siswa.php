c57725bed877ddfd3d055f44a8fc868f114d3649
Bahtiar <zamkorong12@gmail.com>
2019-11-30 10:37:07 +0700

    mengubah method edit

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 Class Siswa extends CI_Controller
 {
     public function __construct()
@@ -12,7 +14,6 @@ Class Siswa extends CI_Controller
         $this->load->model('Siswa_model');
         $this->load->model('Kelas_model');
         $this->load->library('form_validation');        
-        $this->load->helper('url');
     }
     
     public function index()
@@ -26,40 +27,33 @@ Class Siswa extends CI_Controller
 
     public function hapus($id=null)
     {
-        if (!isset($id)){
-            redirect('siswa');
-        }
-        else{
-            $this->Siswa_model->delete($id);
+        if (!isset($id)) redirect('siswa');
+        
+        if($this->Siswa_model->delete($id)){
             redirect('siswa');
         }
     }
 
-    public function ubah($id=null)
+    public function ubah($id = null)
     {
+        if (!isset($id)) redirect('siswa');
+
         $siswa = $this->Siswa_model;
         $kelas =$this->Kelas_model;        
+        $validasi = $this->form_validation;
+        $validasi->set_rules($siswa->rules());
 
+        if ($validasi->run()){
+            $siswa->perbarui();
+            $this->session->set_flashdata('success', 'Berhasil');
+        }
+        $data['siswa'] = $siswa->getByid($id);
         $data["title"] = "Ubah Data";
         $data["actor"] = "Siswa";
         $data["kelass"] = $kelas->getAll();
-        $data['siswa'] = $siswa->getByid($id);
+
         
-        $this->form_validation->set_rules('siswa_nis','NIS','required|numeric');
-        $this->form_validation->set_rules('siswa_nisn','NISN','required|numeric');
-        $this->form_validation->set_rules('siswa_nama','NAMA','required');
-
-
-        if ($this->form_validation->run() == FALSE){
-            $this->load->view('siswa/ubah',$data);
-        } else{
-
-            if(isset($_POST["submit"])) {
-                $siswa->perbarui();
-                $this->session->set_flashdata('flash', 'Ditambahkan');
-            }
-            redirect('siswa');
-        }
+        $this->load->view('siswa/ubah',$data);
     }
 
     public function tambah()
@@ -72,7 +66,7 @@ Class Siswa extends CI_Controller
         $data["kelass"] = $kelas->getAll();
 
         $this->form_validation->set_rules('siswa_nis','NIS','required|numeric');
-        $this->form_validation->set_rules('siswa_nisn','NISN','required|numeric');
+        $this->form_validation->set_rules('siswa_nik','NIK','required|numeric');
         $this->form_validation->set_rules('siswa_nama','NAMA','required');
 
 
@@ -83,24 +77,13 @@ Class Siswa extends CI_Controller
 
             if(isset($_POST["submit"])) {
                 $siswa->simpan();
-                $uploadFoto = $this->upload_foto_siswa();
-                $this->Model_siswa->save($uploadFoto);
+                //$uploadFoto = $this->upload_foto_siswa();
+                //$this->Model_siswa->save($uploadFoto);
                 $this->session->set_flashdata('flash', 'Ditambahkan');
             }
             redirect('siswa');
         }
     }
 
-    function upload_foto_siswa(){
-        $config['upload_path']          = './uploads/';
-        $config['allowed_types']        = 'jpg|png';
-        $config['max_size']             = 1024; // imb
-        $this->load->library('upload', $config);
-            // proses upload
-        $this->upload->do_upload('siswa_foto');
-        $upload = $this->upload->data();
-        return $upload['file_name'];
-    }
-    
 
 }
