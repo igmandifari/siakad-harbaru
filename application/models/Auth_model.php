c078bfeae9971e6a8d3865580e1ac76fbccaaaef
Bahtiar <zamkorong12@gmail.com>
2019-12-08 05:35:44 +0700

    change hash

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -21,7 +21,7 @@ class Auth_model extends CI_Model {
 
     public function cekLogin($username,$password){
         $this->db->where('admin_username',$username);
-        $this->db->where('admin_password',md5($password));
+        $this->db->where('admin_password',md5(sha1($password)));
         $user = $this->db->get('admin')->row_array();
         return $user;
 
