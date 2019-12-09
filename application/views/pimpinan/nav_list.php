defd58ed52d04198cbd499255070011129a5ac07
Igman Difari <54891762+igmandifari@users.noreply.github.com>
2019-12-09 08:59:31 +0700

    add tahun ajaran

--- application/views/pimpinan/nav_list.php
+++ application/views/pimpinan/nav_list.php
@@ -117,6 +117,24 @@
                                 </li>
                             </ul>
                         </li>
+                        <li class="nav-main-item">
+                            <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                                <i class="nav-main-link-icon si si-energy"></i>
+                                <span class="nav-main-link-name">Tahun Ajaran</span>
+                            </a>
+                            <ul class="nav-main-submenu">
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link" href="<?=base_url('tahunajaran/');?>">
+                                        <span class="nav-main-link-name">Daftar Tahun Ajaran</span>
+                                    </a>
+                                </li>
+                                <li class="nav-main-item">
+                                    <a class="nav-main-link" href="<?=base_url('tahunajaran/tambah');?>">
+                                        <span class="nav-main-link-name">Tambah Tahun Ajaran</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
                 <!-- END Side Navigation -->
