b27fe7add5f01e2e16d21341a79ffec68438080c
Bahtiar <zamkorong12@gmail.com>
2019-12-22 11:51:48 +0700

    n

--- application/views/dasbor/dasbor_tutor.php
+++ application/views/dasbor/dasbor_tutor.php
@@ -12,15 +12,15 @@
 
         <!-- Icons -->
         <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-        <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+        <link rel="shortcut icon" href="<?=base_url('assets/media/favicons/favicon.png')?>">
+        <link rel="icon" type="image/png" sizes="192x192" href="<?=base_url('assets/media/favicons/favicon-192x192.png')?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('assets/media/favicons/apple-touch-icon-180x180.png')?>">
         <!-- END Icons -->
 
         <!-- Stylesheets -->
         <!-- Fonts and OneUI framework -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
-        <link rel="stylesheet" id="css-main" href="assets/css/oneui.min.css">
+        <link rel="stylesheet" id="css-main" href="<?=base_url('assets/css/oneui.min.css')?>">
 
         <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
         <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
@@ -301,7 +301,7 @@
             assets/js/core/jquery.appear.min.js
             assets/js/core/js.cookie.min.js
         -->
-        <script src="assets/js/oneui.core.min.js"></script>
+        <script src="<?=base_url('assets/js/oneui.core.min.js')?>"></script>
 
         <!--
             OneUI JS
@@ -309,12 +309,12 @@
             Custom functionality including Blocks/Layout API as well as other vital and optional helpers
             webpack is putting everything together at assets/_es6/main/app.js
         -->
-        <script src="assets/js/oneui.app.min.js"></script>
+        <script src="<?=base_url('assets/js/oneui.app.min.js')?>"></script>
 
         <!-- Page JS Plugins -->
-        <script src="assets/js/plugins/chart.js/Chart.bundle.min.js"></script>
+        <script src="<?=base_url('assets/js/plugins/chart.js/Chart.bundle.min.js')?>"></script>
 
         <!-- Page JS Code -->
-        <script src="assets/js/pages/be_pages_dashboard.min.js"></script>
+        <script src="<?=base_url('assets/js/pages/be_pages_dashboard.min.js')?>"></script>
     </body>
 </html>
