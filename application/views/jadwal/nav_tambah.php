0b8dc4723ff57001849c3e9ab89c22aa8fbf8947
Bahtiar <zamkorong12@gmail.com>
2020-01-17 10:12:59 +0700

    add nav feedback

--- application/views/jadwal/nav_tambah.php
+++ application/views/jadwal/nav_tambah.php
@@ -137,6 +137,12 @@
                             </ul>
                         </li>
                         <li class="nav-main-item">
+                            <a class="nav-main-link" href="<?=base_url('masukan')?>">
+                            <i class="nav-main-link-icon fab fa-rocketchat ml-1"></i>
+                                <span class="nav-main-link-name">Data Masukan</span>
+                            </a>
+                        </li>
+                        <li class="nav-main-item">
                             <a class="nav-main-link" href="<?=base_url('pengaturan')?>">
                             <i class="nav-main-link-icon si si-settings"></i>
                                 <span class="nav-main-link-name">Pengaturan</span>
