bb85a5170262f437f2ca58bbf5343fcd5a2e6c60
Bahtiar <zamkorong12@gmail.com>
2020-01-17 10:08:19 +0700

    add join with wargabelajar and add delete

--- application/models/Masukan_model.php
+++ application/models/Masukan_model.php
@@ -11,5 +11,15 @@ class Masukan_model extends CI_Model {
 
 		return $this->db->query("SELECT * FROM masukan WHERE tahunajaran_id='$tahun' AND wargabelajar_id='$id' ORDER BY created_at DESC")->result_array();
 	}
+	public function getAll()
+	{
+		
+		return $this->db->query("SELECT masukan.masukan_id,masukan.masukan,masukan.created_at,masukan.wargabelajar_id,wargabelajar.wargabelajar_nama FROM masukan INNER JOIN wargabelajar ON wargabelajar.wargabelajar_id=masukan.wargabelajar_id")->result_array();
+	}
+	public function hapus($id)
+	{
+		$this->db->where('masukan_id',$id);
+		return $this->db->delete('masukan');
+	}
 
 }
