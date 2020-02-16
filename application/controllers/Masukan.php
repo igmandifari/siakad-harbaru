bb482e6718eb47754a52a65edde9adb2990b2de0
Bahtiar <zamkorong12@gmail.com>
2020-02-16 11:23:08 +0700

    fix: masukan redirect if admin

--- application/controllers/Masukan.php
+++ application/controllers/Masukan.php
@@ -6,6 +6,7 @@ Class Masukan extends CI_Controller{
 		parent::__construct();
         $url=base_url();
         if($this->session->userdata('MASUK') != TRUE)redirect($url);
+        if($this->session->userdata('level') == 0) redirect("rekapmasukan");
         if($this->session->userdata('level') != 1) redirect("dasbor");
         
 		$this->load->model('Masukan_model');
